c616b748de751201693540aa52847665656288fc
Ross Burton <ross@linux.intel.com>
2009-04-14 10:44:08 +0100

    Add oauth test case using the OAuth test server

---
+++ tests/oauth.c
@@ -0,0 +1,85 @@
+/*
+ * librest - RESTful web services access
+ * Copyright (c) 2008, 2009, Intel Corporation.
+ *
+ * Authors: Rob Bradford <rob@linux.intel.com>
+ *          Ross Burton <ross@linux.intel.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include <rest/oauth-proxy.h>
+#include <rest/oauth-proxy-private.h>
+#include <stdio.h>
+
+int
+main (int argc, char **argv)
+{
+  RestProxy *proxy;
+  OAuthProxy *oproxy;
+  OAuthProxyPrivate *priv;
+  RestProxyCall *call;
+  GError *error = NULL;
+
+  g_thread_init (NULL);
+  g_type_init ();
+
+  /* Create the proxy */
+  proxy = oauth_proxy_new ("key", "secret",
+                           "http://term.ie/oauth/example/%s",
+                           TRUE);
+  oproxy = OAUTH_PROXY (proxy);
+  g_assert (oproxy);
+  priv = PROXY_GET_PRIVATE (oproxy);
+
+  /* First stage authentication, this gets a request token */
+  oauth_proxy_auth_step (oproxy, "request_token.php");
+  g_assert_cmpstr (priv->token, ==, "requestkey");
+  g_assert_cmpstr (priv->token_secret, ==, "requestsecret");
+
+  /* Second stage authentication, this gets an access token */
+  oauth_proxy_auth_step (OAUTH_PROXY (proxy), "access_token");
+
+  g_assert_cmpstr (priv->token, ==, "accesskey");
+  g_assert_cmpstr (priv->token_secret, ==, "accesssecret");
+
+  /* Make some test calls */
+  rest_proxy_bind (proxy, "echo_api.php");
+
+  call = rest_proxy_new_call (proxy);
+  rest_proxy_call_add_param (call, "foo", "bar");
+  if (!rest_proxy_call_run (call, NULL, &error))
+    g_error ("Cannot make call: %s", error->message);
+  g_assert_cmpstr (rest_proxy_call_get_payload (call), ==, "foo=bar");
+  g_object_unref (call);
+
+  call = rest_proxy_new_call (proxy);
+  rest_proxy_call_add_param (call, "numbers", "1234567890");
+  if (!rest_proxy_call_run (call, NULL, &error))
+    g_error ("Cannot make call: %s", error->message);
+  g_assert_cmpstr (rest_proxy_call_get_payload (call), ==, "numbers=1234567890");
+  g_object_unref (call);
+
+  call = rest_proxy_new_call (proxy);
+  rest_proxy_call_add_param (call, "escape", "!£$%^&*()");
+  if (!rest_proxy_call_run (call, NULL, &error))
+    g_error ("Cannot make call: %s", error->message);
+  g_assert_cmpstr (rest_proxy_call_get_payload (call), ==, "escape=%21%C2%A3%24%25%5E%26%2A%28%29");
+  g_object_unref (call);
+
+  g_object_unref (proxy);
+
+  return 0;
+}
