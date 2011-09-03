b8eecfc131619a2c5752e1644f3df011b45ef4b4
Damien Lespiau <damien.lespiau@intel.com>
2011-09-03 16:53:24 +0100

    build: Make it possible to autogen without gtk-doc
    On Windows, I don't want to go through installing gtk-doc just to get some
    DLLs compiled.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-gtkdocize --flavour no-tmpl || exit 1
+GTKDOCIZE=`which gtkdocize`
+if test -z $GTKDOCIZE; then
+  echo "*** No gtk-doc support ***"
+  echo "EXTRA_DIST =" > gtk-doc.make
+  echo "CLEANFILES =" >> gtk-doc.make
+else
+  gtkdocize --flavour no-tmpl || exit 1
+fi
+
 ACLOCAL="${ACLOCAL-aclocal} $ACLOCAL_FLAGS" autoreconf -v -i && ./configure $@
