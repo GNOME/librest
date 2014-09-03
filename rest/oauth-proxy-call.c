b50ace7738ea03817acdad87fb2b338a86018329
Christophe Fergeau <cfergeau@redhat.com>
2014-09-03 11:31:49 +0200

    oauth: Add missing include
    This fixes a compilation warning about a missing prototype.

--- rest/oauth-proxy-call.c
+++ rest/oauth-proxy-call.c
@@ -25,6 +25,7 @@
 #include <rest/rest-proxy-call.h>
 #include "oauth-proxy-call.h"
 #include "oauth-proxy-private.h"
+#include "rest-proxy-call-private.h"
 #include "sha1.h"
 
 G_DEFINE_TYPE (OAuthProxyCall, oauth_proxy_call, REST_TYPE_PROXY_CALL)
