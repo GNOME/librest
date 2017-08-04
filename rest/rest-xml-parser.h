964a28a80a773bd1c971d184e93032ec79b5253c
Debarshi Ray <debarshir@gnome.org>
2017-08-04 14:05:22 +0200

    rest-xml-parser: Use G_DECLARE_DERIVABLE_TYPE
    https://bugzilla.gnome.org/show_bug.cgi?id=785811

--- rest/rest-xml-parser.h
+++ rest/rest-xml-parser.h
@@ -29,31 +29,11 @@
 G_BEGIN_DECLS
 
 #define REST_TYPE_XML_PARSER rest_xml_parser_get_type()
+G_DECLARE_DERIVABLE_TYPE (RestXmlParser, rest_xml_parser, REST, XML_PARSER, GObject)
 
-#define REST_XML_PARSER(obj) \
-  (G_TYPE_CHECK_INSTANCE_CAST ((obj), REST_TYPE_XML_PARSER, RestXmlParser))
-
-#define REST_XML_PARSER_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_CAST ((klass), REST_TYPE_XML_PARSER, RestXmlParserClass))
-
-#define REST_IS_XML_PARSER(obj) \
-  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), REST_TYPE_XML_PARSER))
-
-#define REST_IS_XML_PARSER_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_TYPE ((klass), REST_TYPE_XML_PARSER))
-
-#define REST_XML_PARSER_GET_CLASS(obj) \
-  (G_TYPE_INSTANCE_GET_CLASS ((obj), REST_TYPE_XML_PARSER, RestXmlParserClass))
-
-typedef struct {
-  GObject parent;
-} RestXmlParser;
-
-typedef struct {
+struct _RestXmlParserClass {
   GObjectClass parent_class;
-} RestXmlParserClass;
-
-GType rest_xml_parser_get_type (void);
+};
 
 RestXmlParser *rest_xml_parser_new (void);
 RestXmlNode *rest_xml_parser_parse_from_data (RestXmlParser *parser,
