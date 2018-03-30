613ad8ab28d8ab921ac78039d0fa99cc6a234796
Corentin Noël <corentin@elementary.io>
2018-03-30 09:47:49 +0100

    header: add rest.h as unique header
    This allows easier use of the library

---
+++ rest/rest.h
@@ -0,0 +1,36 @@
+/*
+ * librest - RESTful web services access
+ * Copyright (c) 2008, 2009, 2010 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef _REST_H
+#define _REST_H 1
+
+#include <rest/oauth2-proxy.h>
+#include <rest/oauth2-proxy-call.h>
+#include <rest/oauth-proxy.h>
+#include <rest/oauth-proxy-call.h>
+#include <rest/rest-enum-types.h>
+#include <rest/rest-param.h>
+#include <rest/rest-params.h>
+#include <rest/rest-proxy.h>
+#include <rest/rest-proxy-auth.h>
+#include <rest/rest-proxy-call.h>
+#include <rest/rest-xml-node.h>
+#include <rest/rest-xml-parser.h>
+
+#endif
