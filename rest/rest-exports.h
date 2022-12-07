c231c1d20b208596fb8b948ec1c622dc8dc22bbd
Chun-wei Fan <fanchunwei@src.gnome.org>
2022-12-07 13:10:03 +0800

    librest: Add a header used for exporting symbols
    ...with compiler directives such as __declspec(dllexport) or GCC visibility
    macros.  Define the export macro as nothing unless it is overridden during
    the build.
    
    Also define a macro for marking deprecated symbols so that they can be exported
    in the same manner, too.

---
+++ rest/rest-exports.h
@@ -0,0 +1,32 @@
+/*
+ * librest - RESTful web services access
+ * Copyright (c) 2008, 2009, Intel Corporation.
+ *
+ * Authors: Rob Bradford <rob@linux.intel.com>
+ *          Ross Burton <ross@linux.intel.com>
+ *          Chun-wei Fan <fanc999@yahoo.com.tw>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#pragma once
+
+#include <glib.h>
+
+#ifndef REST_EXPORT
+# define REST_EXPORT
+#endif
+
+#define REST_DEPRECATED G_DEPRECATED REST_EXPORT
